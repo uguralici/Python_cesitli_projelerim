647b256a56846a4e1e8cdab8402ed66531bc703a
cenkdarihan <darihancenk58@gmail.com>
2023-10-12 01:08:45 +0300

    Beden kitle endeksini hesaplayan program
    Bu program girilen kilo ve boy değerlerine göre kullanıcının beden kitle endeksini hesaplar.

---
+++ Beden kitle endeksini bulan program/Beden kitle endeksini hesaplayan program.py
@@ -0,0 +1,10 @@
+#Beden kitle endeksini hesaplayan program
+while True:
+ print(" ")
+ print("Beden kitle endeksini hesaplayan programa hoşgeldiniz, lütfen kilo ve boy değerlerini giriniz.")
+ print(" ")
+ kilo=float(input("Kilo değeri: "))
+ boy=float(input("Boy değeri (metre cinsinden): "))
+ bedenkitleendeksi= kilo / (boy ** 2)
+ print(" ")
+ print("{}" "{:.2f}".format("Beden kitle endeksi: ", bedenkitleendeksi))
