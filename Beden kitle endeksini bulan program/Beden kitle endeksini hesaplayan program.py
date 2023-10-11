5f4be184d874570838475405c7e4981bf3f4b009
uguralici <147086778+uguralici@users.noreply.github.com>
2023-10-12 02:48:26 +0300

    Update Beden kitle endeksini hesaplayan program.py
    Bu kod'da eklediklerim boy kilo endeksini sayısal verilerden metinsel açıklamalara döktüm

--- Beden kitle endeksini bulan program/Beden kitle endeksini hesaplayan program.py
+++ Beden kitle endeksini bulan program/Beden kitle endeksini hesaplayan program.py
@@ -8,3 +8,15 @@ while True:
  bedenkitleendeksi= kilo / (boy ** 2)
  print(" ")
  print("{}" "{:.2f}".format("Beden kitle endeksi: ", bedenkitleendeksi))
+  if bedenkitleendeksi >= 40:
+    print("morbid obez")
+ elif 39.9> bedenkitleendeksi >= 30 :
+    print("obez")
+ elif 29.9> bedenkitleendeksi >=25:
+    print("İdeal Kilonun Üstü")
+    
+ elif 24.9 > bedenkitleendeksi >= 18.5 :
+    print("İdeal Kilo ")
+    
+ elif 18.5 > bedenkitleendeksi :
+    print("İdeal Kilonun Altı ")
